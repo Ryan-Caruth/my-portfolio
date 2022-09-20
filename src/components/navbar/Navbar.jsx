1e6505db64ac1ad38e1be30a00c40bd53fee4741
Ryan-Caruth <ryan.caruth2@gmail.com>
2022-09-20 09:28:44 -0600

    Created template for navbar

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,9 +1,26 @@
-import React from 'react'
+import React from "react";
+import "./navbar.css";
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
-  )
-}
+    <div className="nav">
+      <div className="logo">
+        logo
+        <span className="social-icons">icons</span>
+      </div>
+        <nav>
+          <ul className="sections">
+            <li>About</li>
+            <hr />
+            <li>Skills</li>
+            <hr />
+            <li>Projects</li>
+            <hr />
+            <li>Contact</li>
+          </ul>
+        </nav>
+    </div>
+  );
+};
 
-export default Navbar
+export default Navbar;
