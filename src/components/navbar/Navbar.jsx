ebba0a46bccc654dcc16f67853c10f372792c4fa
Ryan-Caruth <ryan.caruth2@gmail.com>
2022-09-22 09:50:24 -0600

    Imported icons into nav for social media

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,26 +1,39 @@
 import React from "react";
+import { FaGithubSquare, FaLinkedin, FaEnvelopeSquare } from "react-icons/fa";
 import "./navbar.css";
 
 const Navbar = () => {
   return (
     <div className="nav">
-      <div className="logo">
-      <div className="top-logo">RC</div>
-        <hr />
-        <div className="bottom-logo">Portfolio</div>
+      <div className="nav-left">
+        <div className="logo">
+          <div className="top-logo">RC</div>
+          <hr />
+          <div className="bottom-logo">Portfolio</div>
+        </div>
+        <div className="social-icons">
+          <div className="github-logo">
+            <FaGithubSquare />
+          </div>
+          <div className="linkedin-logo">
+            <FaLinkedin />
+          </div>
+          <div className="email-logo">
+            <FaEnvelopeSquare />
+          </div>
+        </div>
       </div>
-      <div className="social-icons">icons</div>
-      <nav>
-        <ul className="sections">
-          <li>About</li>
-          <hr />
-          <li>Skills</li>
-          <hr />
-          <li>Projects</li>
-          <hr />
-          <li>Contact</li>
-        </ul>
-      </nav>
+        <nav>
+          <ul className="sections">
+            <li>About</li>
+            <hr />
+            <li>Skills</li>
+            <hr />
+            <li>Projects</li>
+            <hr />
+            <li>Contact</li>
+          </ul>
+        </nav>
     </div>
   );
 };
