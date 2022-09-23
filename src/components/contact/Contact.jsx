28b1a45e00b1b8d833e9106fba336adfb9653c49
Ryan-Caruth <ryan.caruth2@gmail.com>
2022-09-23 10:16:10 -0600

    Added text for contact section

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,9 +1,20 @@
-import React from 'react'
+import React from "react";
+import './contact.css'
 
 const Contact = () => {
   return (
-    <div>Let's connect</div>
-  )
-}
+    <div className="contact-container">
+      <h1>Let's Connect</h1>
+      <div className="contact-text">
+        <p>
+          Please feel free to reach out if you would like to collaborate on
+          projects, chat code or anything really. You can also send me an email
+          at ryan.caruth2@gmail.com. Looking forward to hearing from you. All
+          fields are required.
+        </p>
+      </div>
+    </div>
+  );
+};
 
-export default Contact
+export default Contact;
