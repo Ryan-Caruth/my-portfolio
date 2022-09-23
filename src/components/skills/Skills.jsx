c388da8c3425873ff48ebe640e65745a6ec968bf
Ryan-Caruth <ryan.caruth2@gmail.com>
2022-09-23 10:13:54 -0600

    added list of skills

--- src/components/skills/Skills.jsx
+++ src/components/skills/Skills.jsx
@@ -1,8 +1,28 @@
 import React from 'react'
+import './skills.css'
 
 const Skills = () => {
   return (
-    <div>The technology skills I am good at</div>
+    <div>
+      <h1>My Skills</h1>
+      <ul>
+        <li>HTML5</li>
+        <li>Socket.IO</li>
+        <li>Trello</li>
+        <li>Git</li>
+        <li>CSS3</li>
+        <li>Material UI</li>
+        <li>Figma</li>
+        <li>VS Code</li>
+        <li>JavaScript</li>
+        <li>React</li>
+        <li>Github</li>
+        <li>MongoDB</li>
+        <li>Node.js</li>
+        <li>Express.js</li>
+        <li>Python</li>
+      </ul>
+    </div>
   )
 }
 
