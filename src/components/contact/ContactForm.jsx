0e5b78f7c02b46203c38439c70d1840c93e9eea6
Ryan-Caruth <ryan.caruth2@gmail.com>
2022-09-25 09:34:41 -0600

    Added divs to contact form for better styling

--- src/components/contact/ContactForm.jsx
+++ src/components/contact/ContactForm.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { useState } from "react";
+import "./contactForm.css";
 
 const ContactForm = () => {
   const [firstName, setFirstName] = useState();
@@ -10,52 +11,65 @@ const ContactForm = () => {
 
   return (
     <form>
-      <label htmlFor="firstName">First Name: </label>
-      <input
-        type="string"
-        id="firstName"
-        autoComplete="off"
-        required
-        value={firstName}
-        onChange={(e) => setFirstName(e.target.event)}
-      />
-      <label htmlFor="lastName">Last Name: </label>
-      <input
-        type="string"
-              id="lastName"
-        autoComplete="off"
-        required
-        value={lastName}
-        onChange={(e) => setLastName(e.target.event)}
-      />
-      <label htmlFor="email">Email: </label>
-      <input
-        type="email"
-        id="email"
-        autoComplete="off"
-        required
-        value={email}
-        onChange={(e) => setEmail(e.target.event)}
-      />
-      <label htmlFor="subject">Subject: </label>
-      <input
-        type="string"
-        id="subject"
-        autoComplete="off"
-        required
-        value={subject}
-        onChange={(e) => setSubject(e.target.event)}
-      />
-      <label htmlFor="message">Message: </label>
-      <textarea
-        type="textBox"
-              id="message"
-              rows='20'
-              cols='80'
-        required
-        value={message}
-        onChange={(e) => setMessage(e.target.event)}
-      />
+      <div className="name">
+        <label htmlFor="firstName">First Name: </label>
+        <input
+          type="string"
+          id="firstName"
+          autoComplete="off"
+          required
+          value={firstName}
+          onChange={(e) => setFirstName(e.target.event)}
+        />
+        <label htmlFor="lastName">Last Name: </label>
+        <input
+          type="string"
+          id="lastName"
+          autoComplete="off"
+          required
+          value={lastName}
+          onChange={(e) => setLastName(e.target.event)}
+        />
+      </div>
+      <div className="email">
+        <label htmlFor="email">Email: </label>
+        <input
+          type="email"
+          id="email"
+          autoComplete="off"
+          required
+          value={email}
+          onChange={(e) => setEmail(e.target.event)}
+        />
+      </div>
+      <div className="subject">
+        <label htmlFor="subject">Subject: </label>
+        <input
+          type="string"
+          id="subject"
+          autoComplete="off"
+          required
+          value={subject}
+          onChange={(e) => setSubject(e.target.event)}
+        />
+      </div>
+      <div className="message">
+        <label htmlFor="message">Message: </label>
+        <textarea
+          type="textBox"
+          id="message"
+          rows="20"
+          cols="80"
+          required
+          value={message}
+          onChange={(e) => setMessage(e.target.event)}
+        />
+      </div>
+      <div className="submitButton">
+        <button id="submit" type="submit">
+          Submit
+        </button>
+      </div>
     </form>
   );
 };
